ff55656a250b4d0a9f6760898367a591474bfd26
Kartik <kitrakiar73@gmail.com>
2024-09-16 23:01:02 +0530

    added the pizzaStore file in the factory pattern

---
+++ The Factory Pattern/pizzaStore.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+
+
+# create the abstract pizza store
+class PizzaStore(ABC):
+    @abstractmethod
+    def create_pizza(self, pizza_type: str):
+        pass
+
+    def order_pizza(self, pizza_type: str) -> Pizza:
+        pizza: Pizza
+
+        pizza = self.create_pizza(pizza_type)
+
+        pizza.prepare()
+        pizza.bake()
+        pizza.cut()
+        pizza.box()
+
+        return pizza
+
+
+# create concrete pizza stores
+class NYPizzaStore(PizzaStore):
+    pizza: Pizza
+
+    def create_pizza(self, pizza_type: str):
+        if pizza_type == 'cheese':
+            pizza = NYCheesePizza()
+        elif pizza_type == 'veggie':
+            pizza = NYVeggiePizza()
+        elif pizza_type == 'pepperoni':
+            pizza = NYPepperoniPizza()
+        else:
+            print("Sorry! We don't serve that pizza YET ;)")
+
+        return pizza
+
+
+class ChicagoPizzaStore(PizzaStore):
+    pizza: Pizza
+
+    def create_pizza(self, pizza_type: str):
+        if pizza_type == 'cheese':
+            pizza = ChicagoCheesePizza()
+        elif pizza_type == 'veggie':
+            pizza = ChicagoVeggiePizza()
+        elif pizza_type == 'pepperoni':
+            pizza = ChicagoPepperoniPizza()
+        else:
+            print("Sorry! We don't serve that pizza YET ;)")
+
+        return pizza

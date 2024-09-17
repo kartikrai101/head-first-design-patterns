969886208c2d714e27216eecc13720bf31e9ac03
Kartik <kitrakiar73@gmail.com>
2024-09-17 21:08:11 +0530

    practicing factory pattern

--- Practice/observers.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-# create an interface for the observers
-class Observer(ABC):
-    @abstractmethod
-    def update(self, temperature: float, pressure: float, humidity: float):
-        pass
---
+++ Practice/pizza_store.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+
+class PizzaStore(ABC):
+    pizza: Pizza
+
+    @abstractmethod
+    def create_pizza(self, pizza_type: str) -> Pizza:
+        pass
+
+    def order_pizza(self, pizza_type: str) -> Pizza:
+        # this method will ask the create pizza factory method to create a pizza for it
+        self.pizza = self.create_pizza(pizza_type)
+
+        # now that we have the pizza, we can start doing our pizza operations and return it at the end
+        self.pizza.prepare()
+        self.pizza.bake()
+        self.pizza.cut()
+        self.pizza.box()
+
+        return self.pizza
+
+
+# create concrete pizza stores
+

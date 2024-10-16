25ff06bf286bfeaef0db9ce181bad2d12c934315
Kartik <kitrakiar73@gmail.com>
2024-10-16 22:44:11 +0530

    added code for the waitress class that will print the menus

---
+++ The Iterator Pattern/waitress.py
@@ -0,0 +1,27 @@
+from menus import BreakfastMenu, DinerMenu
+from iterators import Iterator
+
+
+# the waitress is expected to print the breakfast menu or the diner menu without having to consult with the cook
+class Waitress:
+    diner_menu: DinerMenu
+    breakfast_menu: BreakfastMenu
+
+    def __init__(self, diner_menu: DinerMenu, breakfast_menu: BreakfastMenu):
+        self.diner_menu = diner_menu
+        self.breakfast_menu = breakfast_menu
+
+    def print_menu(self):
+        diner_menu_it = self.diner_menu.create_iterator()
+        breakfast_menu_it = self.breakfast_menu.create_iterator()
+        print("---------------------- Here's our today's special breakfast menu: ---------------------- \n")
+        self.display_menu(breakfast_menu_it)
+        print("---------------------- Here's our today's special Lunch menu: ---------------------- \n")
+        self.display_menu(diner_menu_it)
+
+    def display_menu(self, itr: Iterator):
+        while itr.has_next():
+            item = itr.next()
+            print(f"Name: {item.get_name()}")
+            print(f"Description: {item.get_description()}")
+            print(f"Price: {item.get_price()}")

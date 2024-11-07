2f4167103a47d702af98b7f99ed9f143f62f4413
Kartik <kitrakiar73@gmail.com>
2024-11-07 21:08:08 +0530

    updated the duck simulator to use the Duck Factory to get different ducks that it needs

--- The Compound Pattern/duckSimulator.py
+++ The Compound Pattern/duckSimulator.py
@@ -1,22 +1,22 @@
 from ducks import Quackable, MallardDuck, RedHeadDuck, DuckCall, RubberDuck
 from goose import Goose, GooseAdapter
 from duckDecorators import QuackCounter
+from duckFactory import AbstractDuckFactory
 
 
 class DuckSimulator:
     def simulate_duck(self, duck: Quackable):
         duck.quack()
 
-    def simulate(self):
-        quack_counter_wrapper: QuackCounter
-        mallard_duck: Quackable = QuackCounter(MallardDuck())
-        redhead_duck: Quackable = QuackCounter(RedHeadDuck())
-        duck_call: Quackable = QuackCounter(DuckCall())
-        rubber_duck: Quackable = QuackCounter(RubberDuck())
+    def simulate(self, duck_factory: AbstractDuckFactory):
+        mallard_duck: Quackable = duck_factory.create_mallard_duck()
+        redhead_duck: Quackable = duck_factory.create_redhead_duck()
+        duck_call: Quackable = duck_factory.create_duck_call()
+        rubber_duck: Quackable = duck_factory.create_rubber_duck()
         goose: Goose = Goose()
         goose_duck: Quackable = GooseAdapter(goose)
 
-        print("----------------------------------- Duck Simulator(With Decorator) -----------------------------------")
+        print("----------------------------------- Duck Simulator: With Duck Factory -----------------------------------")
 
         self.simulate_duck(mallard_duck)
         self.simulate_duck(redhead_duck)
--- The Compound Pattern/main.py
+++ The Compound Pattern/main.py
@@ -1,9 +1,11 @@
 from duckSimulator import DuckSimulator
+from duckFactory import CountingDuckFactory
 
 
 def main():
     duck_simulator = DuckSimulator()
-    duck_simulator.simulate()
+    duck_factory = CountingDuckFactory()
+    duck_simulator.simulate(duck_factory)
 
 
 if __name__ == "__main__":
